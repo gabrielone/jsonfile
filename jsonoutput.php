7d41e591beb3cfc7f5650fc5ecc1b8e9cc6211b5
gabrielone <gheorghevirgiliugabriel@gmail.com>
2018-02-13 12:50:36 +0200

    Delete jsonoutput.php

--- jsonoutput.php
+++
@@ -1,8 +0,0 @@
-{
-	"id": "1",
-	"name": "Sanktips",
-	"description":"Android Development Tutorials",
-	"facebook":"https:\/\/www.facebook.com\/Sanktips",
-	"youtube":"https:\/\/www.youtube.com\/user\/Sanktips",
-	"googleplus":"https:\/\/www.plus.google.com\/104396495043738353645"
-}
